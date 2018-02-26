cbd99127402459dea8e2636c634b8a4e39b894b2
Ryan Nowakowski <tubaman@fattuba.com>
2018-02-26 11:15:33 -0600

    make test work no matter what menu is expanded

--- test.py
+++ test.py
@@ -6,15 +6,22 @@ from squash import Squash
 
 
 cj = MozillaCookieJar(sys.argv[1])
+base_url = sys.argv[2]
+
 cj.load(ignore_discard=True, ignore_expires=True)
-squash = Squash("http://mysquashserver/squash", cj)
+squash = Squash(base_url, cj)
 libraries = squash.get_libraries()
 for library in libraries:
     print("library: %s" % library['title'])
-    for child in library['children']:
-        print("child: %s" % child['title'])
-        assert child['attr']['rel'] == 'folder'
-        folder = squash.get_folder(child['attr']['resId'])
-        for requirement in folder:
-            print("requirement: %s" % requirement['title'])
-from IPython import embed; embed()
+    if library['attr']['rel'] == 'drive':
+        drive = squash.get_drive(library['attr']['resId'])
+        for child in drive:
+            print("child: %s" % child['title'])
+            if child['attr']['rel'] == 'folder':
+                folder = squash.get_folder(child['attr']['resId'])
+                for requirement in folder:
+                    print("requirement: %s" % requirement['title'])
+            else:
+                print("unknown child rel: %s" % child['attr']['rel'])
+    else:
+        print("unknown library rel: %s" % library['attr']['rel'])
